70fc35f485c6286df05d2652c14a08906dff8ae3
epitaciofontinelle <epitaciofontinelle@live.com>
2022-04-10 23:22:44 -0300

    redux persist

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './store';
+import store, { persistor } from './store';
 import history from './services/history';
 import Routers from './routes';
 import Header from './components/Header';
@@ -11,12 +12,14 @@ import GlobalStyles from './styles/GlobalStyles';
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter history={history}>
-        <ToastContainer autoClose={3000} />
-        <Header />
-        <Routers />
-        <GlobalStyles />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter history={history}>
+          <ToastContainer autoClose={3000} />
+          <Header />
+          <Routers />
+          <GlobalStyles />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 }
